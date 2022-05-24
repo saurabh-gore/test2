1649496d88bd83eca3e44b50a98e4566a50acba4
saurabh-gore <96182895+saurabh-gore@users.noreply.github.com>
2022-05-24 17:29:57 +0530

    Added Build script for happy6

---
+++ h/happy6/happy6_1.3.2_ubi_8.5.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e         # ----------------------------------------------------------------------------         #         # Package       : happy6        # Version       : 1.3.2        # Source repo   : http://asda.git        # Language		:         # Tested on     : ubi_8.5        # Travis-Check  : True        # Script License: Apache License Version 2.0         # Maintainer    : saurabh_gore.comecho "hii"
+echo "zalay"
+echo "pleasee "
