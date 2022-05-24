4f293520145b955e1ad301c82112d499fa4e4fde
saurabh-gore <96182895+saurabh-gore@users.noreply.github.com>
2022-05-24 17:35:54 +0530

    Added Build script for happy7

---
+++ h/happy7/happy7_1.3.2_ubi_8.5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e 
+         # ---------------------------------------------------------------------------- 
+         # 
+         # Package       : happy7
+         # Version       : 1.3.2
+         # Source repo   : http://asda.git
+         # Language		: 
+         # Tested on     : ubi_8.5
+         # Travis-Check  : True
+         # Script License: Apache License Version 2.0 
+         # Maintainer    : saurabh_gore.com
+echo "hii"
+echo "zalay"
+echo "pleasee "
