cc2cefd9e58da739884b4b824d875241abdbc12c
saurabh-gore <96182895+saurabh-gore@users.noreply.github.com>
2022-05-24 18:08:47 +0530

    Added Build script for birthday

---
+++ b/birthday/birthday_1.4.5_ubi_8.5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e          
+# ----------------------------------------------------------------------------         
+#         
+# Package       : birthday         
+# Version       : 1.4.5         
+# Source repo   : http://bith/birthday.com         
+# Language		: Python         
+# Tested on     : ubi_8.5         
+# Travis-Check  : True         
+# Script License: Apache License Version 2.0          
+# Maintainer    : saurabh_gore.com
+         
+# ---------------------------------------------------------------------------- echo "Hiii There"
+echo "zaaalaaay finallay"
+echo "Lord athi"
