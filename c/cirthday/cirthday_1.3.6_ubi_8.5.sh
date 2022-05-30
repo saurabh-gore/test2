99826e77de1572ddc0f955fc7849cac042de79ea
saurabh-gore <96182895+saurabh-gore@users.noreply.github.com>
2022-05-30 12:00:50 +0530

    Added Build script for cirthday

---
+++ c/cirthday/cirthday_1.3.6_ubi_8.5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e          
+# ----------------------------------------------------------------------------         
+#         
+# Package       : cirthday         
+# Version       : 1.3.6         
+# Source repo   : asdsdfaa.git         
+# Language	  : Python         
+# Tested on     : ubi_8.5         
+# Travis-Check  : True         
+# Script License: Apache License Version 2.0          
+# Maintainer    : saurabh_gore.com         
+# Disclaimer: This script has been tested in root mode on given         
+# ==========  platform using the mentioned version of the package.         
+#             It may not work as expected with newer versions of the         
+#             package and/or distribution. In such case, please         
+#             contact Maintainer of this script.         
+# ---------------------------------------------------------------------------- 
+echo "Hii"
