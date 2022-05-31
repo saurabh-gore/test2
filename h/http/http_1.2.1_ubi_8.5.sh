a4dce35db1501d799013eab997821e98e514ea46
saurabh-gore <96182895+saurabh-gore@users.noreply.github.com>
2022-05-31 12:54:52 +0530

    Added Build script for http

---
+++ h/http/http_1.2.1_ubi_8.5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e          
+# ----------------------------------------------------------------------------         
+#         
+# Package       : http         
+# Version       : 1.2.1         
+# Source repo   : http://github.com/http/http.git         
+# Language	  : Python         
+# Tested on     : ubi_8.5         
+# Travis-Check  : True         
+# Script License: Apache License Version 2.0          
+# Maintainer    : saurabh-gore.com         
+# Disclaimer: This script has been tested in root mode on given         
+# ==========  platform using the mentioned version of the package.         
+#             It may not work as expected with newer versions of the         
+#             package and/or distribution. In such case, please         
+#             contact Maintainer of this script.         
+# ---------------------------------------------------------------------------- 
+echo "Hiii"
